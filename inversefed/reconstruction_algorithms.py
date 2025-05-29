d9969ec8fbcfc6a1448b42f0d92cc6a006e99b6c
Lakith Rambukkanage <aclakith@gmail.com>
2025-05-30 06:26:25 +1000

    Add TEMP fix to avoid unused parameters warning

--- inversefed/reconstruction_algorithms.py
+++ inversefed/reconstruction_algorithms.py
@@ -1195,8 +1195,12 @@ class GradientReconstructor():
                     loss = self.loss_fn(*self.model([batch_input, batch_label], feat_out=True))
                 else:
                     loss = self.loss_fn(self.model(batch_input), batch_label)
-                gradient = torch.autograd.grad(loss, self.model.parameters(), create_graph=True)
-                gradient = [grad for grad in gradient]
+                # TODO FIX TEMP fix to avoid unused parameters warning
+                gradient = torch.autograd.grad(loss, self.model.parameters(), create_graph=True, allow_unused=True)
+                for g, p in zip(gradient, self.model.parameters()):
+                    if g is None:
+                        print(f"Parameter {p.shape} is unused in this forward pass.")
+                gradient = [g if g is not None else torch.zeros_like(p) for g, p in zip(gradient, self.model.parameters())]
                 #apply defense
                 if self.config['defense_method'] is not None:
                     if 'noise' in self.config['defense_method']:
