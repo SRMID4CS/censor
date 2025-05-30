b857754a1e4a7a6f226864ad265be84697572989
Lakith Rambukkanage <aclakith@gmail.com>
2025-05-30 16:02:37 +1000

    Fix smart init for text

--- inversefed/reconstruction_algorithms.py
+++ inversefed/reconstruction_algorithms.py
@@ -1156,9 +1156,11 @@ class GradientReconstructor():
             embed = torch.zeros(shape, **self.setup)
             # first token is always [CLS]
             cls_token_embedding = self.config['cls_token_embedding'].detach().clone()
+            cls_token_embedding = cls_token_embedding.unsqueeze(0).unsqueeze(0).detach().clone().repeat(shape[0], shape[1], 1, 1)
             embed[:, :, 0, :] = cls_token_embedding
             # last 1/2 of tokens are [PAD]
             pad_token_embedding = self.config['pad_token_embedding'].detach().clone()
+            pad_token_embedding = pad_token_embedding.unsqueeze(0).unsqueeze(0).detach().clone().repeat(shape[0], shape[1], 1, 1)
             half = shape[2] // 2
             embed[:, :, half:, :] = pad_token_embedding
             embed = embed.to(self.device)
