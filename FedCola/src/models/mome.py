5d6f646eaf7020edf0ac0c105e0f4e02fdfc3687
Lakith Rambukkanage <aclakith@gmail.com>
2025-07-01 13:52:47 +1000

    Add custom model mome_small_patch32

--- FedCola/src/models/mome.py
+++ FedCola/src/models/mome.py
@@ -1078,10 +1078,30 @@ def mome_small_patch8(pretrained, args, **kwargs):
                 )
     model.sync_shared_weights()
     if pretrained:
-        model.pretrain_vit(['vit_small_patch16_224', None])
+        model.pretrain_vit(['vit_small_patch8_224', None])
     return model 
 
+# custom models
+@register_model
+def mome_small_patch32(pretrained, args, **kwargs):
 
+    model = ModalityAgnosticTransformer(img_size=224,
+                patch_size=32,
+                embed_dim=384,
+                depth=12,
+                num_heads=6,
+                vocab_size=args.vocab_size, 
+                max_text_len=args.seq_len,
+                drop_path_rate=args.dropout,
+                shared_param=args.shared_param,
+                share_scope=args.share_scope,
+                colearn_param=args.colearn_param,
+                **kwargs
+                )
+    model.sync_shared_weights()
+    if pretrained:
+        model.pretrain_vit(['vit_small_patch32_224', None])
+    return model 
 
 
 
