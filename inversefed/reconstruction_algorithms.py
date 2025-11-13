Fix gradient closure losses by  modality

inversefed/reconstruction_algorithms.py
@@ -192,6 +192,20 @@ class GradientReconstructor():
         self.mean_std = mean_std
         self.num_images = num_images    
 
+        if self.config['model'] == 'FedCola_IMG':
+            self.modality = 'img'
+        elif self.config['model'] == 'FedCola_TXT':
+            self.modality = 'txt'
+        elif self.config['model'] == 'FedCola_IMG_TXT':
+            if 'img' in self.config['indices'] and 'txt' not in self.config['indices']:
+                self.modality = 'img'
+            elif 'txt' in self.config['indices'] and 'img' not in self.config['indices']:
+                self.modality = 'txt'
+            else:
+                self.modality = 'img_txt'
+        else:
+            self.modality = 'img'
+
         #BN Statistics
         self.bn_layers = []
         if self.config['bn_stat'] > 0:
@@ -652,7 +666,7 @@ class GradientReconstructor():
                 losses = [0, 0, 0, 0, 0, 0, 0] # tv, bn, img_norm, group_lazy, KLD, patch, CLIP
                 optimizer.zero_grad()
 
-                closure = self._gradient_closure(optimizer, _x[trial], self.input_data, labels_opt, losses, indices=self.config['indices'])
+                closure = self._gradient_closure(optimizer, _x[trial], self.input_data, labels_opt, losses, indices=self.config['indices'], modality=self.modality)
                 rec_loss = closure()
 
                 optimizer.step()
@@ -777,9 +791,9 @@ class GradientReconstructor():
                 losses = [0, 0, 0, 0, 0, 0, 0] # tv, bn, img_norm, group_lazy, KLD, patch, CLIP
                 optimizer.zero_grad()
                 self.dummy_z = dummy_z[trial]
-                
 
-                closure = self._gradient_closure(optimizer, _x[trial], self.input_data, labels_opt, losses, indices=self.config['indices'])
+
+                closure = self._gradient_closure(optimizer, _x[trial], self.input_data, labels_opt, losses, indices=self.config['indices'], modality=self.modality)
                 rec_loss = closure()
 
                 optimizer.step()
@@ -1062,7 +1076,7 @@ class GradientReconstructor():
                         loss_detail = []
                         for i in range(self.num_samples):
                             self.dummy_z = torch.Tensor(ng_data[i].value).to(self.device)
-                            closure = self._gradient_closure(optimizer[trial], _x_gen[i], self.input_data, labels_opt, losses, indices=self.config['indices'])
+                            closure = self._gradient_closure(optimizer[trial], _x_gen[i], self.input_data, labels_opt, losses, indices=self.config['indices'], modality=self.modality)
                             rec_loss = closure()
                             loss.append(rec_loss.item())
                             loss_detail.append(losses)   #record every ask's losses
@@ -1071,13 +1085,13 @@ class GradientReconstructor():
                         for z, l in zip(ng_data, loss):
                             optimizer[trial].tell(z, l)
                     elif self.G:
-                        closure = self._gradient_closure(optimizer[trial], _x[trial], self.input_data, labels_opt, losses, indices=self.config['indices'])
+                        closure = self._gradient_closure(optimizer[trial], _x[trial], self.input_data, labels_opt, losses, indices=self.config['indices'], modality=self.modality)
                         rec_loss = optimizer[trial].step(closure)
                         rec_loss = rec_loss.item()
                     else:
                         imgs = _x[trial]
 
-                        closure = self._gradient_closure(optimizer[trial], imgs, self.input_data, labels_opt, losses, indices=self.config['indices'])
+                        closure = self._gradient_closure(optimizer[trial], imgs, self.input_data, labels_opt, losses, indices=self.config['indices'], modality=self.modality)
                         rec_loss = optimizer[trial].step(closure)
                         rec_loss = rec_loss.item()
 
@@ -1268,7 +1282,7 @@ class GradientReconstructor():
 
                     _x_trial = [self.gen_dummy_data(self.G_list2d[trial][k], self.generative_model_name, self.dummy_zs[k], noise=self.noise_zs[k], ys=self.ys_zs[k]).to('cpu') for k in range(self.num_images)]
                     _x[trial] = torch.stack(_x_trial).squeeze(1).to(self.device)
-                    closure = self._gradient_closure(optimizer[trial], _x[trial], self.input_data, labels, losses, indices=self.config['indices'])
+                    closure = self._gradient_closure(optimizer[trial], _x[trial], self.input_data, labels, losses, indices=self.config['indices'], modality=self.modality)
                     rec_loss = optimizer[trial].step(closure)
                     if self.config['lr_decay']:
                         scheduler[trial].step()
@@ -1367,7 +1381,7 @@ class GradientReconstructor():
         else:
             raise ValueError()
 
-    def _gradient_closure(self, optimizer, x_trial, input_gradient, label, losses, indices='def'):
+    def _gradient_closure(self, optimizer, x_trial, input_gradient, label, losses, indices='def', modality='img'):
 
         data_holder = DataHolder()
         def closure():
@@ -1422,7 +1436,7 @@ class GradientReconstructor():
                                                 cost_fn=self.config['cost_fn'], indices=indices,
                                                 weights=self.config['weights'], model = self.model)
 
-                if self.config['total_variation'] > 0 and (self.config['model'] == 'FedCola_IMG' or self.config['model'] == 'FedCola_IMG_TXT'):
+                if self.config['total_variation'] > 0 and (self.config['model'] == 'FedCola_IMG' or self.config['model'] == 'FedCola_IMG_TXT') and ('img' in modality):
                     tv_loss = TV(x_trial)
                     rec_loss += self.config['total_variation'] * tv_loss
                     losses[0] = tv_loss.item()
@@ -1452,7 +1466,7 @@ class GradientReconstructor():
                         rec_loss += self.config['KLD'] * KLD
                         losses[4] = KLD.item()
 
-                if self.config['patch_prior'] > 0 and (self.config['model'] == 'FedCola_IMG' or self.config['model'] == 'FedCola_IMG_TXT'):
+                if self.config['patch_prior'] > 0 and (self.config['model'] == 'FedCola_IMG' or self.config['model'] == 'FedCola_IMG_TXT') and ('img' in modality):
                     patch_prior_loss_value = patch_prior_loss(x_trial, patch_size=self.config['patch_size'])
                     rec_loss += patch_prior_loss_value * self.config['patch_prior']
                     losses[5] = patch_prior_loss_value.item()
@@ -1719,12 +1733,12 @@ class MultimodalJointGradientReconstructor(GradientReconstructor):
                         self.dummy_z = None
     
                     if self.config['img_recon_method'] == 'GAN_free' and iteration < self.img_max_iterations:
-                        image_closure = self._gradient_closure(image_optimizer[trial], x_i_hat_to_opt[trial], self.input_data, x_t_hat_to_opt[trial], image_losses, indices=self.config.get('img_indices'))
+                        image_closure = self._gradient_closure(image_optimizer[trial], x_i_hat_to_opt[trial], self.input_data, x_t_hat_to_opt[trial], image_losses, indices=self.config.get('img_indices'), modality='img')
                         image_rec_loss = image_optimizer[trial].step(image_closure)
                         image_rec_loss = image_rec_loss.item()
 
                     if self.config['txt_recon_method'] == 'GAN_free' and iteration < self.txt_max_iterations:
-                        text_closure = self._gradient_closure(text_optimizer[trial], x_i_hat_to_opt[trial], self.input_data, x_t_hat_to_opt[trial], text_losses, indices=self.config.get('txt_indices'))
+                        text_closure = self._gradient_closure(text_optimizer[trial], x_i_hat_to_opt[trial], self.input_data, x_t_hat_to_opt[trial], text_losses, indices=self.config.get('txt_indices'), modality='txt')
                         text_rec_loss = text_optimizer[trial].step(text_closure)
                         text_rec_loss = text_rec_loss.item()
 