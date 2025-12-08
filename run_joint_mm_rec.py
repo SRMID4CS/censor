94a24267b668db00e5540c9e07b3e415fbbf285a
Lakith Rambukkanage <aclakith@gmail.com>
2025-12-08 22:36:38 +1100

    Fix var name bug

--- run_joint_mm_rec.py
+++ run_joint_mm_rec.py
@@ -583,11 +583,11 @@ if __name__ == "__main__":
                     G = rec_machine.G
 
                 if 'CIFAR' in config['dataset'] or 'Cifar' in config['dataset'] or 'cifar' in config['dataset']:
-                    text_shape = (10, 384)
+                    txt_shape = (10, 384)
                 else:
-                    text_shape = (20, 384)
+                    txt_shape = (20, 384)
 
-                result = rec_machine.reconstruct(input_gradient, None, img_shape=input_shape, dryrun=iter_dryrun, text_shape=text_shape)
+                result = rec_machine.reconstruct(input_gradient, None, img_shape=input_shape, dryrun=iter_dryrun, txt_shape=txt_shape)
                 if iter_dryrun:
                     continue
             else:
@@ -620,11 +620,11 @@ if __name__ == "__main__":
                     logger.info("Real labels:{}".format(labels))
 
                 if 'CIFAR' in config['dataset'] or 'Cifar' in config['dataset'] or 'cifar' in config['dataset']:
-                    text_shape = (10, 384)
+                    txt_shape = (10, 384)
                 else:
-                    text_shape = (20, 384)
+                    txt_shape = (20, 384)
 
-                result = rec_machine.reconstruct(input_parameters, labels_to_support, img_shape=input_shape, dryrun=args.dryrun, text_shape=text_shape)
+                result = rec_machine.reconstruct(input_parameters, labels_to_support, img_shape=input_shape, dryrun=args.dryrun, txt_shape=txt_shape)
 
             #lpips
             lpips_loss = lpips.LPIPS(net='vgg', spatial=False).to(**setup)
